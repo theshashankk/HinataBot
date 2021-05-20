1471a3bff1f3f0224d1b609d420221eccf6ab6a9
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-20 14:00:25 +0530

    Update chatstatus.py

--- Hinatabot/modules/helper_funcs/telethn/chatstatus.py
+++ Hinatabot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
 from Hinatabot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from LaylaRobot import DRAGONS
+from Hinatabot import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
