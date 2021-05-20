07eb3ca1883a1c254e2745fecfa937e606c8db80
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-20 13:49:54 +0530

    Update locks_sql.py

--- Hinatabot/modules/sql/locks_sql.py
+++ Hinatabot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from LaylaRobot.modules.sql import SESSION, BASE
+from Hinatabot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
