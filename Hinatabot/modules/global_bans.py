f10f1042369884b8792022c8d2612876ac3562ef
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-20 16:34:11 +0530

    Update global_bans.py

--- Hinatabot/modules/global_bans.py
+++ Hinatabot/modules/global_bans.py
@@ -7,13 +7,13 @@ import Hinatabot.modules.sql.global_bans_sql as sql
 from Hinatabot import (DEV_USERS, EVENT_LOGS, OWNER_ID, STRICT_GBAN, DRAGONS,
                           SUPPORT_CHAT, SPAMWATCH_SUPPORT_CHAT, DEMONS, TIGERS,
                           WOLVES, sw, dispatcher)
-from LaylaRobot.modules.helper_funcs.chat_status import (is_user_admin,
+from Hinatabot.modules.helper_funcs.chat_status import (is_user_admin,
                                                            support_plus,
                                                            user_admin)
-from LaylaRobot.modules.helper_funcs.extraction import (extract_user,
+from Hinatabot.modules.helper_funcs.extraction import (extract_user,
                                                           extract_user_and_text)
-from LaylaRobot.modules.helper_funcs.misc import send_to_list
-from LaylaRobot.modules.sql.users_sql import get_all_chats
+from Hinatabot.modules.helper_funcs.misc import send_to_list
+from Hinatabot.modules.sql.users_sql import get_all_chats
 from telegram import ParseMode, Update
 from telegram.error import BadRequest, TelegramError
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
