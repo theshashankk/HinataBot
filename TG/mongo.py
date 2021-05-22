4f8ff16fdb2676b8229ea02858a916ed556545b4
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-22 13:24:05 +0530

    Update mongo.py

--- TG/mongo.py
+++ TG/mongo.py
@@ -3,10 +3,10 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from ShasaBot import MONGO_DB_URI 
+from TG import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from ShasaBot.conf import get_int_key, get_str_key
+from TG.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
