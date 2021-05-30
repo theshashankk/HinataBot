73d22d8078fe5f8d9299b59892e015e8c5fbc961
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-30 22:51:22 +0530

    Create music_player.py

---
+++ hinataBot/modules/music_player.py
@@ -0,0 +1,16 @@
+__mod_name__ = "music Player"
+__help__ = """
+***𝙈𝙐𝙎𝙄𝘾 𝙋𝙇𝘼𝙔𝙀𝙍***
+- /play <song name> - play song you requested
+- /dplay <song name> - play song you requested via deezer
+- /splay <song name> - play song you requested via jio saavn
+- /playlist - Show now playing list
+- /current - Show now playing
+- /song <song name> - download songs you want quickly
+- /search <query> - search videos on youtube with details
+- /deezer <song name> - download songs you want quickly via deezer
+- /saavn <song name> - download songs you want quickly via saavn
+- /video <song name> - download videos you want quickly
+
+***©Team Coffin***
+"""
