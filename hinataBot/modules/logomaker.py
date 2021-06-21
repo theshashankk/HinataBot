22df3e1e7f8892076f38ae045f6a3413c932cc9c
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-06-21 17:28:40 +0530

    Update logomaker.py

--- hinataBot/modules/logomaker.py
+++ hinataBot/modules/logomaker.py
@@ -20,23 +20,23 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./hinataBot/resources/blackbg.jpg')
+    img = Image.open('./hinataBot/resources/OP_BG.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./hinataBot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype("./hinataBot/resources/hinata_op.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="blue")
+    draw.text((x, y), text, font=font, fill="red", stroke_width=25, stroke_fill="white")
     fname2 = "LogoByhinata.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By Hinata Vot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By Hinata Bot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
